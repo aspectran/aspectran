aa0d4a9b2f334ef0e28a6c1dfb724a69e37717ab
gulendol <gulendol@gmail.com>
2015-08-26 16:58:31 +0900

    [license update] Update License Headers in Java

--- java/com/aspectran/support/http/HttpAccessControlAllowOriginFilter.java
+++ java/com/aspectran/support/http/HttpAccessControlAllowOriginFilter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.aspectran.support.http;
 
 import java.io.IOException;
@@ -11,8 +26,6 @@ import com.aspectran.core.adapter.RequestAdapter;
 import com.aspectran.core.adapter.ResponseAdapter;
 
 public class HttpAccessControlAllowOriginFilter {
-	
-	private static final String HEADER_ORIGIN = "origin";
 	
 	private boolean withCredentials;
 	
@@ -31,10 +44,8 @@ public class HttpAccessControlAllowOriginFilter {
 			RequestAdapter requestAdapter = translet.getRequestAdapter();
 			HttpServletRequest req = requestAdapter.getAdaptee();
 	
-			boolean allowed = false;
-			
-			String origin = req.getHeader(HEADER_ORIGIN);
-			allowed = origins.contains(origin);
+			String origin = req.getHeader("origin");
+			boolean allowed = origins.contains(origin);
 			
 			if(allowed) {
 				ResponseAdapter responseAdapter = translet.getResponseAdapter();
