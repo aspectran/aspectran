33af8d173214c83b72e6fe77a048986c4e600d37
Juho Jeong <aspectran@gmail.com>
2015-09-16 04:17:53 +0900

    Converted Project to Maven Project

---
+++ src/main/java/com/aspectran/core/util/logging/LogException.java
@@ -0,0 +1,44 @@
+/**
+ *    Copyright 2009-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.aspectran.core.util.logging;
+
+/**
+ * @author Clinton Begin
+ */
+public class LogException extends RuntimeException {
+
+	/**
+	 * @serial
+	 */
+	static final long serialVersionUID = 1007835506486086212L;
+
+	public LogException() {
+		super();
+	}
+
+	public LogException(String message) {
+		super(message);
+	}
+
+	public LogException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public LogException(Throwable cause) {
+		super(cause);
+	}
+
+}
