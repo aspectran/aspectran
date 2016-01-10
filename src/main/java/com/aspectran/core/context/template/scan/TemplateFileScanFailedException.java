e084313d80bfe26c8ac0832bcf70a402394553aa
Juho Jeong <aspectran@gmail.com>
2016-01-10 23:44:14 +0900

    Move package

---
+++ src/main/java/com/aspectran/core/context/template/scan/TemplateFileScanFailedException.java
@@ -0,0 +1,62 @@
+/**
+ *    Copyright 2009-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.aspectran.core.context.template.scan;
+
+import com.aspectran.core.context.bean.BeanException;
+
+/**
+ * The Class TemplateFileScanFailedException.
+ */
+public class TemplateFileScanFailedException extends BeanException {
+
+	/** @serial */
+	private static final long serialVersionUID = -6390411548631589304L;
+
+	/**
+	 * Simple constructor.
+	 */
+	public TemplateFileScanFailedException() {
+		super();
+	}
+
+	/**
+	 * Constructor to create exception with a message.
+	 * 
+	 * @param msg A message to associate with the exception
+	 */
+	public TemplateFileScanFailedException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Constructor to create exception to wrap another exception.
+	 * 
+	 * @param cause The real cause of the exception
+	 */
+	public TemplateFileScanFailedException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Constructor to create exception to wrap another exception and pass a message.
+	 * 
+	 * @param msg The message
+	 * @param cause The real cause of the exception
+	 */
+	public TemplateFileScanFailedException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
