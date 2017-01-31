165f319781bac49f48b12d91933dea15ee629716
Juho Jeong <aspectran@gmail.com>
2017-01-31 23:31:24 +0900

    Moved from package com.aspectran.support

--- src/main/java/com/aspectran/core/support/BasePathFactoryBean.java
+++ src/main/java/com/aspectran/core/support/BasePathFactoryBean.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.aspectran.support;
+package com.aspectran.core.support;
 
 import com.aspectran.core.context.ActivityContext;
 import com.aspectran.core.context.bean.ablility.FactoryBean;
