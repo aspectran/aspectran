317ae619913c2dde20925df964663e86ce9cecda
Juho Jeong <aspectran@gmail.com>
2017-03-06 00:49:56 +0900

    renamed class Application.java to AspectranConsole.java

---
+++ src/main/java/com/aspectran/console/AspectranConsole.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2008-2017 Juho Jeong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.aspectran.console;
+
+import com.aspectran.console.service.ConsoleAspectranService;
+import com.aspectran.core.util.logging.Log;
+import com.aspectran.core.util.logging.LogFactory;
+
+/**
+ * The Aspectran Console Application.
+ *
+ * @since 2016. 1. 17.
+ */
+public class AspectranConsole {
+
+	private static final Log log = LogFactory.getLog(AspectranConsole.class);
+
+	private static final String DEFAULT_ASPECTRAN_CONFIG_FILE = "aspectran-config.apon";
+
+	public static void main(String[] args) {
+		String aspectranConfigFile;
+
+		if (args.length > 0) {
+			aspectranConfigFile = args[0];
+		} else {
+			aspectranConfigFile = DEFAULT_ASPECTRAN_CONFIG_FILE;
+		}
+
+		int exitStatus = 0;
+
+		try {
+			ConsoleAspectranService.build(aspectranConfigFile).service();
+		} catch (Exception e) {
+			e.printStackTrace();
+			exitStatus = 1;
+		}
+
+		System.exit(exitStatus);
+	}
+
+}
