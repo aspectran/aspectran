8b838a54496d2cc70ac5c76031e476349b5fcb95
Juho Jeong <aspectran@gmail.com>
2017-04-17 08:31:24 +0900

    Update EmbeddedAspectranService.java

--- src/main/java/com/aspectran/embedded/service/EmbeddedAspectranService.java
+++ src/main/java/com/aspectran/embedded/service/EmbeddedAspectranService.java
@@ -57,7 +57,6 @@ public class EmbeddedAspectranService extends BasicAspectranService {
 
     public EmbeddedAspectranService() {
         super(new EmbeddedApplicationAdapter());
-        this.sessionAdapter = new EmbeddedSessionAdapter();
     }
 
     @Override
