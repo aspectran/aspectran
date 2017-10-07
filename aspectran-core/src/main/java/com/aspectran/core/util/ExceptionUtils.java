a1c65d505ee4ffd649cbbae2dbee9e9b756182aa
Juho Jeong <aspectran@gmail.com>
2017-10-07 16:52:28 +0900

    Added ExceptionUtils.java

---
+++ aspectran-core/src/main/java/com/aspectran/core/util/ExceptionUtils.java
@@ -0,0 +1,110 @@
+package com.aspectran.core.util;
+
+import java.io.IOException;
+
+/**
+ * Provides utilities for manipulating and examining Throwable objects.
+ *
+ * <p>Created: 2017. 10. 7.</p>
+ *
+ * @since 5.0.0
+ */
+public class ExceptionUtils {
+
+    /**
+     * Method that can be used to find the "root cause", innermost
+     * of chained (wrapped) exceptions.
+     */
+    public static Throwable getRootCause(Throwable t) {
+        while (t.getCause() != null) {
+            t = t.getCause();
+        }
+        return t;
+    }
+
+    /*
+     **********************************************************
+     * Exception handling; simple re-throw
+     **********************************************************
+     */
+
+    /**
+     * Helper method that will check if argument is an {@link Error},
+     * and if so, (re)throw it; otherwise just return
+     */
+    public static Throwable throwIfError(Throwable t) {
+        if (t instanceof Error) {
+            throw (Error) t;
+        }
+        return t;
+    }
+
+    /**
+     * Helper method that will check if argument is an {@link RuntimeException},
+     * and if so, (re)throw it; otherwise just return
+     */
+    public static Throwable throwIfRTE(Throwable t) {
+        if (t instanceof RuntimeException) {
+            throw (RuntimeException) t;
+        }
+        return t;
+    }
+
+    /**
+     * Helper method that will check if argument is an {@link IOException},
+     * and if so, (re)throw it; otherwise just return
+     */
+    public static Throwable throwIfIOE(Throwable t) throws IOException {
+        if (t instanceof IOException) {
+            throw (IOException) t;
+        }
+        return t;
+    }
+
+    /**
+     * Method that works like by calling {@link #getRootCause} and then
+     * either throwing it (if instanceof {@link IOException}), or
+     * return.
+     */
+    public static Throwable throwRootCauseIfIOE(Throwable t) throws IOException {
+        return throwIfIOE(getRootCause(t));
+    }
+
+    /**
+     * Method that will wrap 't' as an {@link IllegalArgumentException} if it
+     * is a checked exception; otherwise (runtime exception or error) throw as is
+     */
+    public static void throwAsIAE(Throwable t) {
+        throwAsIAE(t, t.getMessage());
+    }
+
+    /**
+     * Method that will wrap 't' as an {@link IllegalArgumentException} (and with
+     * specified message) if it
+     * is a checked exception; otherwise (runtime exception or error) throw as is
+     */
+    public static void throwAsIAE(Throwable t, String msg) {
+        throwIfRTE(t);
+        throwIfError(t);
+        throw new IllegalArgumentException(msg, t);
+    }
+
+    /**
+     * Method that will locate the innermost exception for given Throwable;
+     * and then wrap it as an {@link IllegalArgumentException} if it
+     * is a checked exception; otherwise (runtime exception or error) throw as is
+     */
+    public static void unwrapAndThrowAsIAE(Throwable t) {
+        throwAsIAE(getRootCause(t));
+    }
+
+    /**
+     * Method that will locate the innermost exception for given Throwable;
+     * and then wrap it as an {@link IllegalArgumentException} if it
+     * is a checked exception; otherwise (runtime exception or error) throw as is
+     */
+    public static void unwrapAndThrowAsIAE(Throwable t, String msg) {
+        throwAsIAE(getRootCause(t), msg);
+    }
+
+}
