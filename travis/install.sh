1b1c615889f556476a352beb6f2f506b1f1d900d
Juho Jeong <aspectran@gmail.com>
2017-10-10 19:00:24 +0900

    Update travis ci build

--- travis/install.sh
+++ travis/install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-if [ $TRAVIS_JDK_VERSION == "oraclejdk8" ]; then
-  # Java 1.8
-  mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
-fi
+./mvnw install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
