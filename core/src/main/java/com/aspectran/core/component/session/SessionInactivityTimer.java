10467166a730aed430eb75a40056d917a156b402
Juho Jeong <aspectran@gmail.com>
2017-10-15 21:45:42 +0900

    Update license header

--- core/src/main/java/com/aspectran/core/component/session/SessionInactivityTimer.java
+++ core/src/main/java/com/aspectran/core/component/session/SessionInactivityTimer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2017 Juho Jeong
+ * Copyright (c) 2008-2017 The Aspectran Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
