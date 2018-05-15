d93743167dd5eba0b07da55dc6f5235eeaf988fe
Juho Jeong <aspectran@gmail.com>
2018-05-15 18:40:27 +0900

    Improved procrun batch scripts

--- demo/app/bin/procrun/install.bat
+++ demo/app/bin/procrun/install.bat
@@ -1,20 +1,36 @@
 @ECHO OFF
 
+set BASE_DIR=%~dp0..\..
+set SERVICE_NAME=DemoService
+
+@REM Detect java home
 if not defined JAVA_HOME (
   :undefined
   set /p JAVA_HOME=Enter path to JAVA_HOME:
   if not defined JAVA_HOME goto:undefined
 )
 
-set BASE_DIR=%~dp0%..\..
-set SERVICE_NAME=DemoService
+@REM Detect x86 or x64
+IF PROCESSOR_ARCHITECTURE EQU "ia64" GOTO IS_ia64
+IF PROCESSOR_ARCHITEW6432 EQU "ia64" GOTO IS_ia64
+IF PROCESSOR_ARCHITECTURE EQU "amd64" GOTO IS_amd64
+IF PROCESSOR_ARCHITEW6432 EQU "amd64" GOTO IS_amd64
+IF DEFINED ProgramFiles(x86) GOTO IS_amd64
+:IS_x86
 set PR_INSTALL=%BASE_DIR%\bin\procrun\prunsrv.exe
- 
+goto IS_x64End
+:IS_amd64
+set PR_INSTALL=%BASE_DIR%\bin\procrun\prunsrv_amd64.exe
+goto IS_x64End
+:IS_ia64
+set PR_INSTALL=%BASE_DIR%\bin\procrun\prunsrv_ia64.exe
+:IS_x64End
+
 @REM Service log configuration
 set PR_LOGPREFIX=%SERVICE_NAME%
 set PR_LOGPATH=%BASE_DIR%\logs
-set PR_STDOUTPUT=%BASE_DIR%\logs\stdout.txt
-set PR_STDERROR=%BASE_DIR%\logs\stderr.txt
+set PR_STDOUTPUT=
+set PR_STDERROR=
 set PR_LOGLEVEL=Debug
  
 @REM Path to java installation
@@ -22,7 +38,7 @@ set PR_JVM=%JAVA_HOME%\jre\bin\server\jvm.dll
 set PR_CLASSPATH=%BASE_DIR%\lib\*
  
 @REM Startup configuration
-set PR_STARTUP=auto
+set PR_STARTUP=manual
 set PR_STARTMODE=jvm
 set PR_STARTCLASS=com.aspectran.daemon.ProcrunDaemon
 set PR_STARTMETHOD=start
@@ -36,7 +52,7 @@ set PR_STOPMETHOD=stop
 @REM JVM configuration
 set PR_JVMMS=128
 set PR_JVMMX=512
-set PR_JVMSS=4000
+set PR_JVMSS=4096
 set PR_JVMOPTIONS=-Duser.language=en;-Duser.region=US;-Dlogback.configurationFile=%BASE_DIR%\config\logback.xml;-Daspectran.baseDir=%BASE_DIR%
  
 @REM Install service
@@ -65,10 +81,11 @@ set PR_JVMOPTIONS=-Duser.language=en;-Duser.region=US;-Dlogback.configurationFil
   --StopMethod="%PR_STOPMETHOD%"
  
 if not errorlevel 1 goto installed
-echo Failed to install "%SERVICE_NAME%" service.  Refer to log in %PR_LOGPATH%
+echo Failed to install "%SERVICE_NAME%" service.
+echo Refer to log in %PR_LOGPATH%
 goto end
  
 :installed
-echo The Service "%SERVICE_NAME%" has been installed
+echo The Service "%SERVICE_NAME%" has been installed.
  
 :end
--- demo/app/bin/procrun/uninstall.bat
+++ demo/app/bin/procrun/uninstall.bat
@@ -1,1 +1,34 @@
-prunsrv //DS//DemoService
+@ECHO OFF
+
+set BASE_DIR=%~dp0..\..
+set SERVICE_NAME=DemoService
+
+@REM Detect java home
+if not defined JAVA_HOME (
+  :undefined
+  set /p JAVA_HOME=Enter path to JAVA_HOME:
+  if not defined JAVA_HOME goto:undefined
+)
+
+@REM Detect x86 or x64
+IF PROCESSOR_ARCHITECTURE EQU "ia64" GOTO IS_ia64
+IF PROCESSOR_ARCHITEW6432 EQU "ia64" GOTO IS_ia64
+IF PROCESSOR_ARCHITECTURE EQU "amd64" GOTO IS_amd64
+IF PROCESSOR_ARCHITEW6432 EQU "amd64" GOTO IS_amd64
+IF DEFINED ProgramFiles(x86) GOTO IS_amd64
+:IS_x86
+set PR_INSTALL=%BASE_DIR%\bin\procrun\prunsrv.exe
+goto IS_x64End
+:IS_amd64
+set PR_INSTALL=%BASE_DIR%\bin\procrun\prunsrv_amd64.exe
+goto IS_x64End
+:IS_ia64
+set PR_INSTALL=%BASE_DIR%\bin\procrun\prunsrv_ia64.exe
+:IS_x64End
+
+@REM Stop service:
+net stop %SERVICE_NAME% 2>nul
+
+%PR_INSTALL% //DS//%SERVICE_NAME%
+
+echo The Service "%SERVICE_NAME%" has been removed.
