f136ac6811cce01ea276cbc526bcf7bed488611c
Juho Jeong <aspectran@gmail.com>
2018-07-23 20:16:29 +0900

    prepare for next development iteration

--- core/src/main/java/com/aspectran/core/util/Aspectran.java
+++ core/src/main/java/com/aspectran/core/util/Aspectran.java
@@ -31,7 +31,7 @@ public class Aspectran {
                 pkg.getImplementationVersion() != null) {
             VERSION = pkg.getImplementationVersion();
         } else {
-            VERSION = System.getProperty("aspectran.version", "5.1.1");
+            VERSION = System.getProperty("aspectran.version", "5.1.2-SNAPSHOT");
         }
 
         POWERED_BY = "Powered by Aspectran " + VERSION;
