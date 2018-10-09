d720ccdff4967dbcf666a030c1d8492c21f07613
Juho Jeong <aspectran@gmail.com>
2018-10-09 23:54:48 +0900

    Pre-update for aspectran-5.3.0 release

--- core/src/main/java/com/aspectran/core/util/Aspectran.java
+++ core/src/main/java/com/aspectran/core/util/Aspectran.java
@@ -31,7 +31,7 @@ public class Aspectran {
                 pkg.getImplementationVersion() != null) {
             VERSION = pkg.getImplementationVersion();
         } else {
-            VERSION = System.getProperty("aspectran.version", "5.2.3");
+            VERSION = System.getProperty("aspectran.version", "5.3.0");
         }
 
         POWERED_BY = "Powered by Aspectran " + VERSION;
