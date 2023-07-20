d93ef419952f26ee5676784a01fb856e4b1f785a
Juho Jeong <aspectran@gmail.com>
2023-07-20 18:00:36 +0900

    Update

--- core/src/main/java/com/aspectran/core/context/expr/ognl/OgnlMemberAccess.java
+++ core/src/main/java/com/aspectran/core/context/expr/ognl/OgnlMemberAccess.java
@@ -16,23 +16,23 @@
 package com.aspectran.core.context.expr.ognl;
 
 import ognl.MemberAccess;
+import ognl.OgnlContext;
 
 import java.lang.reflect.Member;
-import java.util.Map;
 
 public class OgnlMemberAccess implements MemberAccess {
 
     @Override
-    public Object setup(Map context, Object target, Member member, String propertyName) {
+    public Object setup(OgnlContext context, Object target, Member member, String propertyName) {
         return null;
     }
 
     @Override
-    public void restore(Map context, Object target, Member member, String propertyName, Object state) {
+    public void restore(OgnlContext context, Object target, Member member, String propertyName, Object state) {
     }
 
     @Override
-    public boolean isAccessible(Map context, Object target, Member member, String propertyName) {
+    public boolean isAccessible(OgnlContext context, Object target, Member member, String propertyName) {
         return false;
     }
 
