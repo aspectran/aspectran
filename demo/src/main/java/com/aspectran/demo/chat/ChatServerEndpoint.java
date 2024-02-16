f99a43dd216b163d44f9d50f0f021c1559754a3f
Juho Jeong <aspectran@gmail.com>
2024-02-17 00:10:36 +0900

    WebSocket package integrated into web package

--- demo/src/main/java/com/aspectran/demo/chat/ChatServerEndpoint.java
+++ demo/src/main/java/com/aspectran/demo/chat/ChatServerEndpoint.java
@@ -31,7 +31,7 @@ import com.aspectran.utils.annotation.jsr305.NonNull;
 import com.aspectran.utils.annotation.jsr305.Nullable;
 import com.aspectran.utils.logging.Logger;
 import com.aspectran.utils.logging.LoggerFactory;
-import com.aspectran.websocket.jsr356.AspectranConfigurator;
+import com.aspectran.web.websocket.jsr356.AspectranConfigurator;
 import jakarta.websocket.CloseReason;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
