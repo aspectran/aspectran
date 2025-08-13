c1890cd8c3dbe3b7f505ab0c738de96831061996
Juho Jeong <aspectran@gmail.com>
2025-08-13 16:43:05 +0900

    Document missing javadocs

---
+++ demo/src/main/java/com/aspectran/demo/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2008-present The Aspectran Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Demo application package for the Aspectran framework.
+ * <p>All classes inside this package belong to a lightweight demo
+ * application that showcases key features of the framework. The
+ * demo includes sample components, configuration snippets and
+ * simple integration tests meant for educational purposes.</p>
+ */
+package com.aspectran.demo;
---
+++ logging/src/main/java/com/aspectran/logging/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2008-present The Aspectran Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Provides additional utilities to use Logback as the underlying logging library.
+ * <p>The package contains classes that handle log message formatting, level filtering,
+ * and integration with external logging providers. It also defines common exception types
+ * related to logging operations.</p>
+ */
+package com.aspectran.logging;
