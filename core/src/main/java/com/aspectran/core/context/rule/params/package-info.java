dd6c3bc4de236ef442eb4154507a19a98893d25f
Juho Jeong <aspectran@gmail.com>
2025-09-03 21:23:26 +0900

    Document missing javadocs

---
+++ core/src/main/java/com/aspectran/core/context/rule/params/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2008-present The Aspectran Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Contains parameter classes used for parsing APON (Aspectran Object Notation) configuration.
+ * These classes mirror the structure of the configuration and act as data holders
+ * during the conversion from text-based settings to the final Rule object model.
+ */
+package com.aspectran.core.context.rule.params;
