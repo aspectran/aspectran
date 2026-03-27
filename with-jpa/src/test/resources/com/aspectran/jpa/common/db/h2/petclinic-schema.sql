e240104a5f17d4d706e7bbbf05ef839e14d31753
Juho Jeong <aspectran@gmail.com>
2026-03-27 17:24:00 +0900

    Update

--- with-jpa/src/test/resources/com/aspectran/jpa/common/db/h2/petclinic-schema.sql
+++ with-jpa/src/test/resources/com/aspectran/jpa/common/db/h2/petclinic-schema.sql
@@ -7,39 +7,41 @@
 -- DROP TABLE types IF EXISTS;
 -- DROP TABLE owners IF EXISTS;
 
-CREATE SEQUENCE base_entity_id_seq
+CREATE SEQUENCE IF NOT EXISTS base_entity_id_seq
     START WITH 1
     INCREMENT BY 1
     MINVALUE 1
     NOMAXVALUE;
 
-CREATE TABLE vets (
+CREATE TABLE IF NOT EXISTS vets (
     id         NUMBER CONSTRAINT pk_vets PRIMARY KEY,
     first_name VARCHAR2(30),
     last_name  VARCHAR2(30)
 );
-CREATE INDEX vets_last_name ON vets (last_name);
+CREATE INDEX IF NOT EXISTS vets_last_name ON vets (last_name);
 
-CREATE TABLE specialties (
+CREATE TABLE IF NOT EXISTS specialties (
     id   NUMBER CONSTRAINT pk_specialties PRIMARY KEY,
     name VARCHAR2(80)
 );
-CREATE INDEX specialties_name ON specialties (name);
+CREATE INDEX IF NOT EXISTS specialties_name ON specialties (name);
 
-CREATE TABLE vet_specialties (
+CREATE TABLE IF NOT EXISTS vet_specialties (
     vet_id       NUMBER NOT NULL,
     specialty_id NUMBER NOT NULL
 );
+ALTER TABLE vet_specialties DROP CONSTRAINT IF EXISTS fk_vet_specialties_vets;
 ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
+ALTER TABLE vet_specialties DROP CONSTRAINT IF EXISTS fk_vet_specialties_specialties;
 ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_specialties FOREIGN KEY (specialty_id) REFERENCES specialties (id);
 
-CREATE TABLE types (
+CREATE TABLE IF NOT EXISTS types (
     id   NUMBER CONSTRAINT pk_types PRIMARY KEY,
     name VARCHAR2(80)
 );
-CREATE INDEX types_name ON types (name);
+CREATE INDEX IF NOT EXISTS types_name ON types (name);
 
-CREATE TABLE owners (
+CREATE TABLE IF NOT EXISTS owners (
     id         NUMBER CONSTRAINT pk_owners PRIMARY KEY,
     first_name VARCHAR2(30),
     last_name  VARCHAR2(30),
@@ -47,24 +49,27 @@ CREATE TABLE owners (
     city       VARCHAR2(80),
     telephone  VARCHAR2(20)
 );
-CREATE INDEX owners_last_name ON owners (last_name);
+CREATE INDEX IF NOT EXISTS owners_last_name ON owners (last_name);
 
-CREATE TABLE pets (
+CREATE TABLE IF NOT EXISTS pets (
     id         NUMBER CONSTRAINT pk_pets PRIMARY KEY,
     name       VARCHAR2(30),
     birth_date DATE,
     type_id    NUMBER NOT NULL,
     owner_id   NUMBER
 );
+ALTER TABLE pets DROP CONSTRAINT IF EXISTS fk_pets_owners;
 ALTER TABLE pets ADD CONSTRAINT fk_pets_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+ALTER TABLE pets DROP CONSTRAINT IF EXISTS fk_pets_types;
 ALTER TABLE pets ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
-CREATE INDEX pets_name ON pets (name);
+CREATE INDEX IF NOT EXISTS pets_name ON pets (name);
 
-CREATE TABLE visits (
+CREATE TABLE IF NOT EXISTS visits (
     id          NUMBER CONSTRAINT pk_visits PRIMARY KEY,
     pet_id      NUMBER,
     visit_date  DATE,
     description VARCHAR2(255)
 );
+ALTER TABLE visits DROP CONSTRAINT IF EXISTS fk_visits_pets;
 ALTER TABLE visits ADD CONSTRAINT fk_visits_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
-CREATE INDEX visits_pet_id ON visits (pet_id);
+CREATE INDEX IF NOT EXISTS visits_pet_id ON visits (pet_id);
